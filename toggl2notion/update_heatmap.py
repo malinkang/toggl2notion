c9461f80c764a32b43ed8384574058706e10bf2a
malinkang <malinkang@adsconflux.com>
2026-02-10 22:41:42 +0800

    refactor: 优化 workflow 结构 (参考 flomo 配置) 并支持 heatmap 目录隔离

--- toggl2notion/update_heatmap.py
+++ toggl2notion/update_heatmap.py
@@ -4,17 +4,21 @@ from .utils import get_embed, log, upload_image
 from .notion_helper import NotionHelper
 
 def get_file():
+    activation_code = os.getenv("ACTIVATION_CODE")
+    if not activation_code:
+        activation_code = "default"
+        
     # 设置文件夹路径
-    folder_path = './OUT_FOLDER'
+    folder_path = f'./heatmap/{activation_code}'
 
     # 检查文件夹是否存在
     if os.path.exists(folder_path) and os.path.isdir(folder_path):
-        entries = os.listdir(folder_path)
+        entries = [f for f in os.listdir(folder_path) if f.endswith(".svg")]
         
         file_name = entries[0] if entries else None
         return file_name
     else:
-        log("OUT_FOLDER does not exist.")
+        log(f"{folder_path} does not exist.")
         return None
     
 def main():
@@ -25,7 +29,7 @@ def main():
         if not activation_code:
             activation_code = "default"
             
-        svg_path = f"./OUT_FOLDER/{image_file}"
+        svg_path = f"./heatmap/{activation_code}/{image_file}"
         # 使用 upload_image 上传
         image_url = upload_image(activation_code, svg_path)
         
